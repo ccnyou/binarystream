9623d78d4d007a1ca220d61c4af58bbd8b72eba8
ccnyou <ccnyou@qq.com>
2016-05-07 19:04:16 +0800

    + init commit
    完成基础的output功能，mac + windows 测试通过

---
+++ binarystream/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  binarystream
+//
+//  Created by 聪宁陈 on 16/5/7.
+//  Copyright © 2016年 ccnyou. All rights reserved.
+//
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "binarystream.h"
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cc::binarystream bss;
+    bss << 0x20 << '\x20' << endl;
+    const vector<char> &v = bss.vector();
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        cout << (int)*it << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
